74117e61f115014908ded9a63957610d039d7e53
q-miyata <miyata@q-co.jp>
2025-03-27 13:06:43 +0900

    エラー通知をslackに送る

--- e2e/google.spec.ts
+++ e2e/google.spec.ts
@@ -3,4 +3,6 @@ import { test, expect } from '@playwright/test';
 test('Googleトップにアクセスできる', async ({ page }) => {
   await page.goto('https://www.google.com');
   await expect(page).toHaveTitle(/Google/);
+  //わざとエラーを起こす
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
